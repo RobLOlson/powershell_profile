0d2189bc962671cfa30335b88f7a3548f8dff1e2
Rob L Olson <rob.louis@gmail.com>
2022-12-20 02:35:47 -0900

    add lsd alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -38,9 +38,21 @@ else {
 Remove-Alias -Name cat
 New-Alias -Name cat -Value rich
 
-#
-# New-Alias -Name pactivate -Value '."$(poetry env info --path)\Scripts\activate.ps1"'
-# New-Alias -Name pactivate -Value '(. "$(poetry env info --path)\Scripts\activate.ps1")'
+
+# use ls DELUXE (config file located at "%appdata%\lsd") d/l @ https://github.com/Peltoche/lsd
+Remove-Alias -Name ls
+New-Alias -Name ls -Value lsd
+function ll {
+  lsd --long $args
+}
+function la {
+  lsd --all $args
+}
+function lal {
+  lsd --long --all $args
+}
+
+
 
 function email {
   py -m robo.rob.email $args
@@ -302,7 +314,134 @@ Function calc {
 }
 
 
-
-
 # powertoys keyboard remap, copy as 'default.json' to %appdata%../local/Microsoft/Powertoys/Keyboard... etc.
 # {"remapKeys":{"inProcess":[{"originalKeys":"20","newRemapKeys":"13"},{"originalKeys":"36","newRemapKeys":"20"}]},"remapShortcuts":{"global":[{"originalKeys":"164;49","newRemapKeys":"48"},{"originalKeys":"164;50","newRemapKeys":"57"},{"originalKeys":"164;51","newRemapKeys":"56"},{"originalKeys":"164;52","newRemapKeys":"55"},{"originalKeys":"164;53","newRemapKeys":"109"},{"originalKeys":"164;54","newRemapKeys":"187"},{"originalKeys":"164;65","newRemapKeys":"37"},{"originalKeys":"164;66","newRemapKeys":"78"},{"originalKeys":"164;67","newRemapKeys":"188"},{"originalKeys":"164;68","newRemapKeys":"39"},{"originalKeys":"164;69","newRemapKeys":"73"},{"originalKeys":"164;70","newRemapKeys":"74"},{"originalKeys":"164;72","newRemapKeys":"37"},{"originalKeys":"164;74","newRemapKeys":"40"},{"originalKeys":"164;75","newRemapKeys":"38"},{"originalKeys":"164;76","newRemapKeys":"39"},{"originalKeys":"164;81","newRemapKeys":"80"},{"originalKeys":"164;82","newRemapKeys":"85"},{"originalKeys":"164;83","newRemapKeys":"40"},{"originalKeys":"164;86","newRemapKeys":"77"},{"originalKeys":"164;87","newRemapKeys":"38"},{"originalKeys":"164;88","newRemapKeys":"190"},{"originalKeys":"164;90","newRemapKeys":"191"},{"originalKeys":"164;112","newRemapKeys":"79"},{"originalKeys":"164;113","newRemapKeys":"76"},{"originalKeys":"164;114","newRemapKeys":"75"},{"originalKeys":"164;192","newRemapKeys":"8"},{"originalKeys":"164;160;53","newRemapKeys":"160;189"},{"originalKeys":"164;160;54","newRemapKeys":"107"},{"originalKeys":"164;160;65","newRemapKeys":"160;186"},{"originalKeys":"164;160;112","newRemapKeys":"160;219"},{"originalKeys":"164;160;113","newRemapKeys":"160;221"},{"originalKeys":"164;160;192","newRemapKeys":"46"},{"originalKeys":"162;164;65","newRemapKeys":"162;37"},{"originalKeys":"162;164;68","newRemapKeys":"162;39"},{"originalKeys":"162;164;72","newRemapKeys":"162;37"},{"originalKeys":"162;164;74","newRemapKeys":"162;40"},{"originalKeys":"162;164;75","newRemapKeys":"162;38"},{"originalKeys":"162;164;76","newRemapKeys":"162;39"},{"originalKeys":"162;164;83","newRemapKeys":"162;40"},{"originalKeys":"162;164;87","newRemapKeys":"162;38"},{"originalKeys":"162;164;112","newRemapKeys":"219"},{"originalKeys":"162;164;113","newRemapKeys":"221"},{"originalKeys":"91;187","newRemapKeys":"91;13"}],"appSpecific":[]}}
+
+# Starship Prompt BEGIN
+# VVVVVVVVVVVVVVVVVVVVV
+
+Invoke-Expression (&starship init powershell)
+
+function Invoke-Starship-TransientFunction {
+  &starship module character
+}
+
+function Invoke-Starship-PreCommand {
+  $origLastExitCode = $LASTEXITCODE
+  $exit_code = $?
+  # Write-Host $global:gitpromptvalues.DollarQuestion -NoNewline
+
+  # Ran as Administrator?
+  $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+  $bgcolor = "White"
+
+  If ($IsAdmin) { $bgcolor = "Yellow" }
+
+  If ($exit_code) {} Else { $bgcolor = "Red" }
+
+  if ($env:VIRTUAL_ENV) { $bgcolor = "Green" }
+
+  $tasks = Get-Content -Path ($appdata + '\robolson\tick\tasks.txt') -ErrorAction SilentlyContinue
+
+  # Get path as an array of strings
+  $leaf = Split-Path -leaf -path (Get-Location)
+  $path = @(Split-Path -path (Get-Location)).Split('\') + $leaf
+  $path = @($path | Where-Object { $_ })
+
+  $drive = $path[0]
+
+  # below not used?? delete if nothing breaks
+  # $terminal_height = (Get-Host).UI.RawUI.MaxWindowSize.Height
+  $terminal_width = (Get-Host).UI.RawUI.MaxWindowSize.Width
+
+  $folders = Get-ChildItem -Path (Get-Location) -Directory -ErrorAction SilentlyContinue | Select-Object Name
+
+  $folders = $folders.name
+
+  if (!$folders) { $folders = "" }
+
+  # Hide hidden folders
+  $folders = $folders | Where-Object { $_[0] -ne '.' }
+
+  # below not used??  Delete if nothing breaks
+  # $folders_list = @($folders)
+  $folders = $folders -join ", "
+
+  # Adjust environment folder display if exists
+  if ($env:VIRTUAL_ENV) {
+    if ($folders.contains((Split-path -path $env:VIRTUAL_ENV -leaf))) {
+      $folders = $folders.replace((Split-Path -path $env:VIRTUAL_ENV -leaf), ("[*" + (Split-Path -path $env:VIRTUAL_ENV -leaf) + "]"))
+      $skip_env_line = $true
+    }
+    if ((Split-path -path (Get-Location)) -eq (Split-path -path $env:VIRTUAL_ENV)) {
+      $skip_env_line = $true
+    }
+  }
+
+  # IF folder list requires multiple lines
+  if ( ($folders.length) -gt ($terminal_width)) {
+    $folders = $folders | word_wrap
+
+    $MAX_FOLDER_LINES = 3
+
+    $folders = $folders[0..$MAX_FOLDER_LINES]
+    if ($folders[$MAX_FOLDER_LINES]) {
+      # omit excess folders with '...'
+      $folders[$MAX_FOLDER_LINES] = " " * (($terminal_width - 3) / 2 - 1) + "..."
+    }
+
+    # Pad the end of each line so that the background color changes appropriately
+    $i = 0
+    foreach ($line in $folders) {
+      $folders[$i] = $line + " " * ($terminal_width - $line.length)
+      $i = $i + 1
+    }
+
+    # Lines must be manually joined with new-line characters to print properly
+    $folders = $folders -join "`n"
+
+  }
+  else {
+    $folders = $folders + " " * ($terminal_width - $folders.length)
+  }
+
+
+  # If CWD is too long, try using '~'
+  if (($path -join '\').length -gt $terminal_width -and "users" -in $path) {
+    $shortpath = @('~') + @($path[3..256])
+    if (($shortpath -join '\').length -lt $terminal_width) {
+      $path = $shortpath
+    }
+  }
+
+  # If CWD is STILL too long, use C:\..N..\
+  $cut_count = 1
+  while (($path -join '\').length -gt $terminal_width) {
+    $cut_count += 1
+    $cut_string = '..' + $cut_count + '..'
+    $path = @($drive, $cut_string) + $path[3..256]
+  }
+
+  If ($isadmin) {
+    $Host.ui.rawui.windowtitle = "$leaf [ADMIN]"
+  }
+  Else {
+    $Host.ui.rawui.windowtitle = "$leaf"
+  }
+
+  if ($tasks) {
+    $tasks = $tasks -join ", "
+    if ($tasks.length -gt $terminal_width) {
+      $tasks = $tasks[0..($terminal_width - 4)] -join ""
+      $tasks = $tasks + "..."
+    }
+    $tasks = $tasks + " " * ($terminal_width - $tasks.length)
+    Write-Host $tasks -NoNewline -BackgroundColor Red -ForegroundColor Black
+  }
+
+  Write-Host $folders -NoNewline -BackgroundColor $bgcolor -ForegroundColor Black
+}
+
+# ^^^^^^^^^^^^^^^^^^^
+# STARSHIP PROMPT END
